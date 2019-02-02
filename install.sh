75bdbdb51d98da53a5927bb4c1e6d17d4217a390
mainmachine <dave@b-sharpmusic.com>
2019-02-01 23:50:09 -0500

    refine a few things

--- install.sh
+++ install.sh
@@ -8,5 +8,7 @@ fi
 cp usr/local/bin/rx580-power-control /usr/local/bin/rx580-power-control
 cp etc/systemd/system/rx580-power-control.service /etc/systemd/system/rx580-power-control.service
 
+systemctl daemon-reload
+
 systemctl start rx580-power-control.service
 #systemctl enable rx580-power-control.service
