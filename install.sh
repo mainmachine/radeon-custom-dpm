f4bae3533af3d9b2370f6236c854398efae91153
mainmachine <dave@b-sharpmusic.com>
2019-02-01 22:48:37 -0500

    populate install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo_text "You need to be root to run this script..."
+   exit 1
+fi
+
+cp usr/local/bin/rx580-power-control /usr/local/bin/rx580-power-control
+cp etc/systemd/system/rx580-power-control.service /etc/systemd/system/rx580-power-control.service
+
+systemctl start rx580-power-control.service
+#systemctl enable rx580-power-control.service
